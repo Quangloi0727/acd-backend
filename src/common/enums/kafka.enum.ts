6a3fc21b0332433778817eb2d816a36e1a79b9ce
loinq <loinq@metechvn.com>
2023-06-23 11:51:44 +0700

    update

--- src/common/enums/kafka.enum.ts
+++ src/common/enums/kafka.enum.ts
@@ -9,6 +9,7 @@ export const enum KAFKA_TOPIC_MONITOR {
   CONVERSATION_MESSAGE_SEND = 'ACD.Conversation.Message.Send',
   CONVERSATION_ASSIGN = 'ACD.Conversation.Assign',
   CONVERSATION_PICK = 'ACD.Conversation.Pick',
+  CONVERSATION_JOIN = 'ACD.Conversation.Join',
   CONVERSATION_CLOSE = 'ACD.Conversation.Close',
   CONVERSATION_REOPEN = 'ACD.Conversation.Reopen',
   CONVERSATION_GENERAL_REPORT = 'ACD.Conversation.General.Report'
