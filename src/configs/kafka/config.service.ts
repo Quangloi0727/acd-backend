2ac27086d35e7b102376f93cec6e587826cbb129
__Luong <dot@luongmb.local>
2023-10-20 08:32:19 +0700

    fixed issue multiple brokers

--- src/configs/kafka/config.service.ts
+++ src/configs/kafka/config.service.ts
@@ -1,71 +1,77 @@
-import { Injectable } from '@nestjs/common';
-import { CommonConfigService } from '../config.service';
+import {Injectable} from '@nestjs/common';
+import {CommonConfigService} from '../config.service';
 
 @Injectable()
 export class KafkaConfigService {
-  constructor(private commonConfigService: CommonConfigService) {}
+	constructor(private commonConfigService: CommonConfigService) {
+	}
 
-  get broker(): string {
-    return this.commonConfigService.getString('KAFKA_BROKER', 'broker:9092');
-  }
+	get brokers(): string[] {
+		const brokers = this.commonConfigService.getString('KAFKA_BROKER', 'broker:9092');
+		if (brokers && brokers.indexOf(',') > 0) {
+			return brokers.split(',').map(broker => broker.trim());
+		}
 
-  get registry_host(): string {
-    return this.commonConfigService.getString(
-      'KAFKA_REGISTRY',
-      'http://registry:8081',
-    );
-  }
+		return [brokers];
+	}
 
-  get client_id(): string {
-    return this.commonConfigService.getString('KAFKA_CLIENT_ID', 'acd-backend');
-  }
+	get registry_host(): string {
+		return this.commonConfigService.getString(
+			'KAFKA_REGISTRY',
+			'http://registry:8081',
+		);
+	}
 
-  get group_id(): string {
-    return this.commonConfigService.getString('KAFKA_GROUP_ID', 'acd-backend');
-  }
+	get client_id(): string {
+		return this.commonConfigService.getString('KAFKA_CLIENT_ID', 'acd-backend');
+	}
 
-  get connection_timeout(): number {
-    return this.commonConfigService.getNumber('KAFKA_CONNECTION_TIMEOUT', 3000);
-  }
+	get group_id(): string {
+		return this.commonConfigService.getString('KAFKA_GROUP_ID', 'acd-backend');
+	}
 
-  get authentication_timeout(): number {
-    return this.commonConfigService.getNumber(
-      'KAFKA_AUTHENTICATION_TIMEOUT',
-      1000,
-    );
-  }
+	get connection_timeout(): number {
+		return this.commonConfigService.getNumber('KAFKA_CONNECTION_TIMEOUT', 3000);
+	}
 
-  get reauthentication_timeout(): number {
-    return this.commonConfigService.getNumber(
-      'KAFKA_REAUTHENTICATION_THRESHOLD',
-      10000,
-    );
-  }
+	get authentication_timeout(): number {
+		return this.commonConfigService.getNumber(
+			'KAFKA_AUTHENTICATION_TIMEOUT',
+			1000,
+		);
+	}
 
-  get transaction_timeout(): number {
-    return this.commonConfigService.getNumber(
-      'KAFKA_TRANSACTION_TIMEOUT',
-      30000,
-    );
-  }
+	get reauthentication_timeout(): number {
+		return this.commonConfigService.getNumber(
+			'KAFKA_REAUTHENTICATION_THRESHOLD',
+			10000,
+		);
+	}
 
-  get response_timeout(): number {
-    return this.commonConfigService.getNumber('KAFKA_RESPONSE_TIMEOUT', 30000);
-  }
+	get transaction_timeout(): number {
+		return this.commonConfigService.getNumber(
+			'KAFKA_TRANSACTION_TIMEOUT',
+			30000,
+		);
+	}
 
-  get sasl_enable(): boolean {
-    return this.commonConfigService.getBoolean('KAFKA_SASL_ENABLED', false);
-  }
+	get response_timeout(): number {
+		return this.commonConfigService.getNumber('KAFKA_RESPONSE_TIMEOUT', 30000);
+	}
 
-  get username(): string {
-    return this.commonConfigService.getString('KAFKA_SASL_USERNAME');
-  }
+	get sasl_enable(): boolean {
+		return this.commonConfigService.getBoolean('KAFKA_SASL_ENABLED', false);
+	}
 
-  get password(): string {
-    return this.commonConfigService.getString('KAFKA_SASL_PASSWORD');
-  }
+	get username(): string {
+		return this.commonConfigService.getString('KAFKA_SASL_USERNAME');
+	}
 
-  get mechanism(): string {
-    return this.commonConfigService.getString('KAFKA_SASL_MECHANISM');
-  }
+	get password(): string {
+		return this.commonConfigService.getString('KAFKA_SASL_PASSWORD');
+	}
+
+	get mechanism(): string {
+		return this.commonConfigService.getString('KAFKA_SASL_MECHANISM');
+	}
 }
--- src/providers/kafka/provider.module.ts
+++ src/providers/kafka/provider.module.ts
@@ -1,53 +1,54 @@
-import { Module } from '@nestjs/common';
-import { KafkaConfigModule, KafkaConfigService } from '../../configs';
-import { KafkaModule } from './kafka.module';
-import { KafkaResponseDeserializer } from './deserializer/kafka-response.deserializer';
-import { KafkaRequestSerializer } from './serializer/kafka-request.serializer';
-import { KafkaModuleOption } from './interfaces';
+import {Module} from '@nestjs/common';
+import {KafkaConfigModule, KafkaConfigService} from '../../configs';
+import {KafkaModule} from './kafka.module';
+import {KafkaResponseDeserializer} from './deserializer/kafka-response.deserializer';
+import {KafkaRequestSerializer} from './serializer/kafka-request.serializer';
+import {KafkaModuleOption} from './interfaces';
 
 export const KafkaClientService = 'KAFKA_SERVICE';
 
 @Module({
-  imports: [
-    KafkaModule.registerAsync([KafkaClientService], {
-      useFactory: async (config: KafkaConfigService) => [
-        {
-          name: KafkaClientService,
-          options: {
-            client: {
-              brokers: [config.broker],
-              connectionTimeout: config.connection_timeout,
-              authenticationTimeout: config.authentication_timeout,
-              reauthenticationThreshold: config.reauthentication_timeout,
-              autoConnect: true,
-              requestTimeout: config.transaction_timeout,
-              consumeFromBeginning: true,
-              sasl: config.sasl_enable
-                ? {
-                    mechanism: config.mechanism,
-                    username: config.username,
-                    password: config.password,
-                  }
-                : null,
-            },
-            producer: {
-              allowAutoTopicCreation: true,
-              transactionTimeout: config.transaction_timeout,
-            },
-            consumer: {
-              allowAutoTopicCreation: true,
-              groupId: config.group_id,
-              consumeFromBeginning: true,
-            },
-            responseDefaultTimeout: config.response_timeout,
-            deserializer: new KafkaResponseDeserializer(),
-            serializer: new KafkaRequestSerializer(),
-          },
-        } as KafkaModuleOption,
-      ],
-      inject: [KafkaConfigService],
-      imports: [KafkaConfigModule],
-    }),
-  ],
+	imports: [
+		KafkaModule.registerAsync([KafkaClientService], {
+			useFactory: async (config: KafkaConfigService) => [
+				{
+					name: KafkaClientService,
+					options: {
+						client: {
+							brokers: config.brokers,
+							connectionTimeout: config.connection_timeout,
+							authenticationTimeout: config.authentication_timeout,
+							reauthenticationThreshold: config.reauthentication_timeout,
+							autoConnect: true,
+							requestTimeout: config.transaction_timeout,
+							consumeFromBeginning: true,
+							sasl: config.sasl_enable
+								? {
+									mechanism: config.mechanism,
+									username: config.username,
+									password: config.password,
+								}
+								: null,
+						},
+						producer: {
+							allowAutoTopicCreation: true,
+							transactionTimeout: config.transaction_timeout,
+						},
+						consumer: {
+							allowAutoTopicCreation: true,
+							groupId: config.group_id,
+							consumeFromBeginning: true,
+						},
+						responseDefaultTimeout: config.response_timeout,
+						deserializer: new KafkaResponseDeserializer(),
+						serializer: new KafkaRequestSerializer(),
+					},
+				} as KafkaModuleOption,
+			],
+			inject: [KafkaConfigService],
+			imports: [KafkaConfigModule],
+		}),
+	],
 })
-export class KafkaProviderModule {}
+export class KafkaProviderModule {
+}
